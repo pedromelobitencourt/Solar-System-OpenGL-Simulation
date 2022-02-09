65c951d8cb3c821539a6b2630ec1718bb58ece79
pedrinv <89912712+pedrinv@users.noreply.github.com>
2022-02-08 21:30:34 -0300

    Update carregartextura.h

--- carregartextura.h
+++ carregartextura.h
@@ -1,3 +1,17 @@
+/*
+Membros do grupo:
+    Pedro Vitor Melo Bitencourt (20213001540)
+    Rafael Pereira Duarte (20213005146)
+    Pedro Veloso Inácio de Oliveira (20213004837)
+    Sérgio Henrique Mendes de Assis (20213005182)
+
+Orientador:
+    Glender Brás
+
+Texturas usadas:
+    https://www.solarsystemscope.com/textures/
+*/
+
 #ifndef CARREGAR_TEXTURA_H_INCLUDED
 #define CARREGAR_TEXTURA_H_INCLUDED
 
@@ -15,4 +29,3 @@ public:
 Textura* carregarImagemBMP(const char* nomeArquivo);
 
 #endif
-
